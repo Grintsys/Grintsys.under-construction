6cd08a2096a10dc1efba3dbd2cc3217720abeec2
DeyviTabora <taborapaz@gmail.com>
2020-03-10 15:25:53 -0600

    Updates

---
+++ precache-manifest.70ee34fd3da0e6b71d69f98c7fc417b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b92638473d000d13a7121111a22502",
+    "url": "/Grintsys.under-construction/index.html"
+  },
+  {
+    "revision": "50e875b89f9569098d37",
+    "url": "/Grintsys.under-construction/static/css/2.0d6b273a.chunk.css"
+  },
+  {
+    "revision": "dcbeace575d589c5f9a3",
+    "url": "/Grintsys.under-construction/static/css/main.5215bdae.chunk.css"
+  },
+  {
+    "revision": "50e875b89f9569098d37",
+    "url": "/Grintsys.under-construction/static/js/2.efdb95dc.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Grintsys.under-construction/static/js/2.efdb95dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcbeace575d589c5f9a3",
+    "url": "/Grintsys.under-construction/static/js/main.b33a9930.chunk.js"
+  },
+  {
+    "revision": "00d6d91ace85589a9987",
+    "url": "/Grintsys.under-construction/static/js/runtime-main.0c61032b.js"
+  },
+  {
+    "revision": "5589ad777ac8187f8006882d3f9e27e6",
+    "url": "/Grintsys.under-construction/static/media/19728.5589ad77.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Grintsys.under-construction/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Grintsys.under-construction/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Grintsys.under-construction/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Grintsys.under-construction/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Grintsys.under-construction/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d9de8111b408a9d916fd4447f51a7833",
+    "url": "/Grintsys.under-construction/static/media/logo4.d9de8111.png"
+  }
+]);
